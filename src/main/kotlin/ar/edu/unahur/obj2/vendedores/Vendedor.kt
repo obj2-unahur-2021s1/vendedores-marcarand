6752c07c0a4b69571ea06bd4d97b954264d368ec
marcarand <marcarand@riseup.net>
2021-04-13 17:07:27 -0300

    Etapa 3 y test

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
@@ -5,13 +5,15 @@ class Certificacion(val esDeProducto: Boolean, val puntaje: Int)
 abstract class Vendedor {
   // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
   // Además, a una MutableList se le pueden agregar elementos
-  val certificaciones = mutableListOf<Certificacion>()
+  val certificaciones = mutableListOf<Certificacion>() // lista - colección del tipo Certificacion
 
   // Definimos el método abstracto.
   // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
+  // En este caso devuelve un booleano (true/false)
   abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
 
   // En las funciones declaradas con = no es necesario explicitar el tipo
+  // En este caso devuelve un booleano (true/false)
   fun esVersatil() =
     certificaciones.size >= 3
       && this.certificacionesDeProducto() >= 1
@@ -22,11 +24,14 @@ abstract class Vendedor {
     certificaciones.add(certificacion)
   }
 
+  // Esta función devuelve un booleano (true/false)
   fun esFirme() = this.puntajeCertificaciones() >= 30
 
+  // Esta función devuelve un número entero: Int
   fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
+  // Esta función devuelve un número entero: Int
   fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
-
+  // Esta función devuelve un número entero, ya que 'puntaje' es tipo Int:
   fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
 }
 
@@ -35,6 +40,8 @@ class VendedorFijo(val ciudadOrigen: Ciudad) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return ciudad == ciudadOrigen
   }
+  // Esta función devuelve un booleano (false)
+  fun esInfluyente() = false
 }
 
 // A este tipo de List no se le pueden agregar elementos una vez definida
@@ -42,10 +49,49 @@ class Viajante(val provinciasHabilitadas: List<Provincia>) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return provinciasHabilitadas.contains(ciudad.provincia)
   }
+  // Esta función devuelve un booleano (true/false)
+  fun esInfluyente() = provinciasHabilitadas.sumBy { it.poblacion } >= 10000000
 }
 
 class ComercioCorresponsal(val ciudades: List<Ciudad>) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return ciudades.contains(ciudad)
   }
+  val provinciasHabilitadas = ciudades.map { it.provincia }.toSet()
+
+  // Esta función devuelve un booleano (true/false)
+  fun esInfluyente() =
+    ciudades.size >= 5
+      || provinciasHabilitadas.size >= 3
+}
+
+
+class CentroDistribucion(val ciudad: Ciudad) {
+  // Es una lista mutable de sub-instancias de Vendedor
+  val vendedores = mutableListOf<Vendedor>()
+
+  // La función devuelve un booleano (true/false)
+  fun vendedorRegistrado(vendedor: Vendedor): Boolean {
+    return vendedores.contains(vendedor)
+  }
+
+  // Esta función no devuelve nada, pero genera un mensaje de error
+  // cuando el vendedor ya está registrado en el centro.
+  fun agregarVendedor(vendedor: Vendedor) {
+    check(!vendedorRegistrado (vendedor)) {
+      "El vendedor ya está registrado para el centro"
+       }
+    vendedores.add(vendedor)
+  }
+  // Esta función devuelve una instancia de Vendedor
+  fun vendedorEstrella() = vendedores.maxBy { it.puntajeCertificaciones() }
+
+  // Esta función devuelve un booleano (true/false)
+  fun puedeCubrir(ciudad: Ciudad) = vendedores.any { it.puedeTrabajarEn(ciudad) }
+
+  // Esta función devuelve una colección de vendedores
+  fun vendedoresGenericos() = vendedores.filter { it.otrasCertificaciones() > 0 }
+
+  // Esta función devuelve un booleano (true/false)
+  fun esRobusto() = vendedores.count { it.esFirme() } >= 3
 }
--- src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
@@ -1,16 +1,44 @@
 package ar.edu.unahur.obj2.vendedores
 
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldHave
+import io.kotest.matchers.shouldNotHave
 
 class VendedorTest : DescribeSpec({
+
+  // declaro dos certificaciones para asignar a varios vendedores
+  val certificacion_5p = Certificacion(false, 5)
+  val certificacion_10p = Certificacion(true,10)
+  val certificacion_20p = Certificacion(true, 20)
+
   val misiones = Provincia(1300000)
   val sanIgnacio = Ciudad(misiones)
 
+  // agrego varias provincias y ciudades para reutilizarlas en varios test
+  // Salta
+  val salta = Provincia(535303) // según Censo 2010
+  val cafayate = Ciudad(salta)
+  val cachi = Ciudad(salta)
+  // Chubut
+  val chubut = Provincia(506668)  // según Censo 2010
+  val comodoroRivadavia = Ciudad(chubut)
+  val esquel = Ciudad(chubut)
+
+  // declaro por anticipado los valores de "Vendedor fijo" para reutilizarlos en otros test
+  val obera = Ciudad(misiones)
+  val vendedorFijo = VendedorFijo(obera)
+  // le agrego una certificación
+  vendedorFijo.agregarCertificacion(certificacion_5p)
+
   describe("Vendedor fijo") {
-    val obera = Ciudad(misiones)
-    val vendedorFijo = VendedorFijo(obera)
+    // val obera = Ciudad(misiones)
+    // val vendedorFijo = VendedorFijo(obera)
 
     describe("puedeTrabajarEn") {
       it("su ciudad de origen") {
@@ -33,6 +61,76 @@ class VendedorTest : DescribeSpec({
       }
       it("una ciudad que no pertenece a una provincia habilitada") {
         viajante.puedeTrabajarEn(villaDolores).shouldBeFalse()
+      }
+    }
+    // agrego test para la función "esInfluyente"
+    describe("esInfluyente") {
+      it("no supera los 10 millones") {
+        viajante.esInfluyente().shouldBeFalse()
+      }
+    }
+  }
+
+  // agrego varios tests para un comercio corresponsal
+  describe("Corresponsal") {
+    val comercio = ComercioCorresponsal(listOf(cafayate, esquel, comodoroRivadavia, sanIgnacio))
+
+    describe("puedeTrabajarEn") {
+      it("una ciudad donde tiene sucursal") {
+        comercio.puedeTrabajarEn(cafayate).shouldBeTrue()
+      }
+      it("una ciudad donde no tiene sucursal") {
+        comercio.puedeTrabajarEn(cachi).shouldBeFalse()
+      }
+    }
+    describe("esInfluyente") {
+      it("tiene al menos sucursales en tres provincias") {
+        comercio.esInfluyente().shouldBeTrue()
+      }
+    }
+  }
+
+  // agrego varios test para un Centro de Distribución
+  describe("CentroDistribucion") {
+    val centroCafayate = CentroDistribucion(cafayate)
+    val vendedor1 = VendedorFijo(comodoroRivadavia)
+    val vendedor2 = Viajante(listOf(chubut, misiones, salta))
+    centroCafayate.agregarVendedor(vendedorFijo)
+    centroCafayate.agregarVendedor(vendedor1)
+
+    describe("agregarVendedor") {
+      it("no registrado") {
+        centroCafayate.agregarVendedor(vendedor2).shouldNotBeNull()
+      }
+      it("no permite agregar vendedor ya registrado") {
+        shouldThrowAny { centroCafayate.agregarVendedor(vendedor2) }
+      }
+    }
+    describe("esRobusto") {
+      it("No tiene al menos tres vendedores firmes") {
+        centroCafayate.esRobusto().shouldBeFalse()
+      }
+    }
+    describe("vendedorGenerico") {
+      it("Hay solo un vendedor genérico: vendedorFijo") {
+        centroCafayate.vendedoresGenericos().shouldBe(listOf(vendedorFijo))
+      }
+    }
+    describe("vendedorEstrella") {
+      vendedor2.agregarCertificacion(certificacion_10p)
+      it("El vendedor2 es estrella") {
+        centroCafayate.vendedorEstrella().shouldBe(vendedor2)
+      }
+    }
+    describe("puedeCubrir") {
+      it("El Centro de Distribución puede cubrir la ciudad de Esquel") {
+        centroCafayate.puedeCubrir(esquel).shouldBeTrue()
+      }
+    }
+    describe("Nuevo vendedorEstrella") {
+      vendedor1.agregarCertificacion(certificacion_20p)
+      it("Ahora el vendedor1 es estrella") {
+        centroCafayate.vendedorEstrella().shouldBe(vendedor1)
       }
     }
   }
